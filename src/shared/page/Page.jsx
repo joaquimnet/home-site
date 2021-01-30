6c8197efd7c67a1dc1eaa63414652024aaeb84b7
Joaquim Neto <joaquimmy@yahoo.com>
2021-01-30 15:13:26 -0300

    feat(Page): layout improvements

--- src/shared/page/Page.jsx
+++ src/shared/page/Page.jsx
@@ -2,7 +2,9 @@ import styled from 'styled-components';
 
 export const Page = styled.div`
   min-height: calc(100vh - 5rem);
-  padding: 1rem 15rem;
+  max-width: 900px;
+  margin: auto;
+  padding: 1rem 4rem;
   display: flex;
   flex-flow: column nowrap;
   justify-content: flex-start;
@@ -17,19 +19,19 @@ export const Page = styled.div`
     font-size: 1.2rem;
   }
 
-  @media only screen and (max-width: 1360px) {
-    padding: 1rem 5rem;
-  }
+  //@media only screen and (max-width: 1360px) {
+  //  padding: 1rem 4rem;
+  //}
 
   @media only screen and (max-width: 1000px) {
-    padding: 1rem 4rem;
+    padding: 1rem 2rem;
   }
 
   @media only screen and (max-width: 768px) {
-    padding: 1rem 3rem;
+    padding: 1rem 1.5rem;
   }
 
   @media only screen and (max-width: 400px) {
-    padding: 1rem 1rem;
+    padding: 1rem 0.5rem;
   }
 `;
