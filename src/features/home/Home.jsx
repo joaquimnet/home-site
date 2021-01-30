50ea0ea5558b89361378caead275c65aaafb90f2
Joaquim Neto <joaquimmy@yahoo.com>
2021-01-30 00:38:42 -0300

    feat(Home): home page revamp

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { FaGithub, FaTwitter, FaEnvelope, FaBook } from 'react-icons/all';
 
 import { Page } from '../../shared/page/Page';
+import { Button } from '../../shared/button/Button';
 
 const StyledHome = styled(Page)`
-  justify-content: center;
-  align-items: center;
-
   & h1 {
     font-size: 4rem;
     margin-bottom: 2rem;
+    text-shadow: 1px 4px 3px #772020;
   }
 
   @media only screen and (max-width: 736px) {
@@ -20,14 +20,54 @@ const StyledHome = styled(Page)`
   }
 `;
 
+const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-flow: row wrap;
+
+  @media only screen and (max-width: 736px) {
+    justify-content: center;
+    align-items: center;
+  }
+
+  & a {
+    margin: 0 1rem 1rem 0;
+
+    & svg {
+      margin-right: 0.5rem;
+    }
+  }
+`;
+
 export function Home() {
   return (
     <StyledHome>
       <h1 className="text-center">Welcome to my page.</h1>
       <p className="text-center">
-        It isn&apos;t ready yet, so in the mean time why don&apos;t you check
-        out my <Link to="/blog">blog</Link>?
+        It isn&apos;t ready yet, so in the mean time why don&apos;t you check out my{' '}
+        <Link to="/blog">blog</Link>?
       </p>
+      <hr />
+      <ButtonContainer>
+        <Button
+          animated
+          as="a"
+          href="https://twitter.com/joaquimnet_"
+          target="_blank"
+          rel="noopener"
+        >
+          <FaTwitter /> Twitter
+        </Button>
+        <Button animated as="a" href="https://github.com/joaquimnet" target="_blank" rel="noopener">
+          <FaGithub /> Github
+        </Button>
+        <Button animated as="a" href="mailto:joaquimmy@gmail.com" target="_blank" rel="noopener">
+          <FaEnvelope /> Email
+        </Button>
+        <Button animated as={Link} to="/blog">
+          <FaBook /> Blog
+        </Button>
+      </ButtonContainer>
     </StyledHome>
   );
 }
